f293c0e74490c06588a7be68610f6db28d04553c
dhrumil <dhrumilcontractor@gmail.com>
2023-07-03 11:30:21 +0530

    feat/ export products to excel

--- src/pages/dashboard/Products.js
+++ src/pages/dashboard/Products.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Instance } from '../../axios';
 import { Routes } from '../../helpers/routeHelper';
 import { addToCart } from '../../redux/cart/actions';
+import { utils, writeFile } from 'xlsx';
 
 const Products = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -78,12 +79,19 @@ const Products = () => {
 
   const handleAddToCart = async (idx) => dispatch(addToCart(products[idx]));
 
+  const onExport = () => {
+    const ws = utils.json_to_sheet(products);
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, ws, 'Data');
+    writeFile(wb, 'products.xlsx');
+  };
+
   return isLoading ? (
     <div className="flex justify-center">Loading...</div>
   ) : (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-2 lg:max-w-7xl lg:px-8">
-        <div className="flex justify-between my-2">
+        <div className="flex justify-between items-center my-2">
           <div>
             <input
               type="text"
@@ -93,16 +101,25 @@ const Products = () => {
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          {categories.length ? (
-            <select name="category" defaultValue="" value={category} onChange={onCategoryChange}>
-              <option value="">select category</option>
-              {categories.map((cate) => (
-                <option key={cate} name={cate} value={cate}>
-                  {cate}
-                </option>
-              ))}
-            </select>
-          ) : null}
+          <div className="flex items-center gap-1 sm:gap-5">
+            {categories.length ? (
+              <select name="category" defaultValue="" value={category} onChange={onCategoryChange}>
+                <option value="">select category</option>
+                {categories.map((cate) => (
+                  <option key={cate} name={cate} value={cate}>
+                    {cate}
+                  </option>
+                ))}
+              </select>
+            ) : null}
+            <button
+              type="button"
+              className="bg-purple-700 text-white px-4 py-1 rounded hover:bg-purple-600"
+              onClick={onExport}
+            >
+              export
+            </button>
+          </div>
         </div>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product, idx) => (
